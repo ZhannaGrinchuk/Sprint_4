67b704a5a01525839086657518c134cecd65c3fb
Zhanna <zhannain15kop@yandex.ru>
2022-01-11 23:17:42 +0300

    Sprint_4_2

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -3,16 +3,13 @@ package com.example;
 public class Account {
 
     private final String name;
-    boolean onlySingleSubstring(String str, String subStr) {
-        return str.indexOf(subStr) == str.lastIndexOf(subStr);
+
+       public Account(String name) {
+            this.name = name;
         }
 
-    public Account(String name) {
-        this.name = name;
-    }
-
     public boolean checkNameToEmboss() {
-        return name.length() >= 3 && name.length() <= 19 && name.contains(" ") & onlySingleSubstring(name, " ") && !name.startsWith(" ") && !name.endsWith(" ");
+        return name.matches("(?=.{3,19}$)[а-яА-ЯёЁa-zA-Z0-9]+\\s{1}[а-яА-ЯёЁa-zA-Z0-9]+");
     }
 }
 
--- src/test/java/com/example/AccountTest.java
+++ src/test/java/com/example/AccountTest.java
@@ -3,13 +3,15 @@ package com.example;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class AccountTest {
 
-      private final String name;
-      private final boolean expected;
+    private final String name;
+    private final boolean expected;
+
 
     public AccountTest(String name, boolean expected) {
         this.name = name;
@@ -18,28 +20,28 @@ public class AccountTest {
 
     @Parameterized.Parameters
     public static Object[] getData() {
-            return new Object[][]{
-                    {"pr", false},                      //2 символа без пробела
-                    {"p ri", true},                     //3 символа с пробелом
-                    {"1 ТимотиШаламеТимо", true},       // 18 символов включая пробел в середине
-                    {"1 ТимотиШаламеТимот", true},     // 19 символов включая пробел в середине
-                    {"1 45", true},                   //4 символа включая пробел в середине
-                    {"1 ТимотиШаламеТимоти", false},  //20 символов включая пробел в середине
-                    {" ", false},                    //пробел
-                    {"", false},                    //пустая строка
-                    {" privet", false},            //пробел в начале строки
-                    {"privet ", false},           //пробел в конце строки
-                    {"pri   vet", false},         // Более одного пробела в середине
-                    {"ТимотиШаламе", false}     // отсутствующий пробел в середине
+        return new Object[][]{
+                {"pr", false},                      //2 символа без пробела
+                {"p ri", true},                     //3 символа с пробелом
+                {"1 ТимотиШаламеТимо", true},       // 18 символов включая пробел в середине
+                {"1 ТимотиШаламеТимот", true},     // 19 символов включая пробел в середине
+                {"1 45", true},                   //4 символа включая пробел в середине
+                {"1 ТимотиШаламеТимоти", false},  //20 символов включая пробел в середине
+                {" ", false},                    //пробел
+                {"", false},                    //пустая строка
+                {" privet", false},            //пробел в начале строки
+                {"privet ", false},           //пробел в конце строки
+                {"pri  vet", false},         // Более одного пробела в середине
+                {"ТимотиШаламе", false}      // отсутствующий пробел в середине
 
-            };
-        }
+        };
+    }
 
-        @Test
-        public void shouldPrintNameOrNot() {
-            Account account = new Account(name);
-            boolean actual = account.checkNameToEmboss();
-            assertEquals(expected, actual);
+    @Test
+    public void shouldPrintNameOrNot() {
+        Account account = new Account(name);
+        boolean actual = account.checkNameToEmboss();
+        assertEquals(expected, actual);
 
     }
 }
